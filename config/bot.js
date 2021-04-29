f76664e74c4ee1ee03720fc36f19ad7282fca847
lolways <67913723+lolways@users.noreply.github.com>
2021-04-29 19:56:36 -0300

    token add

--- config/bot.js
+++ config/bot.js
@@ -8,7 +8,7 @@ module.exports = {
     },
 
     discord: {
-        token: 'ODM2NjE2MTk3NTQ2MDQ5NTM3.YIgljQ.LGFYASTXxrnrA47SS2IZy6vhD0A',
+        token: 'ODM2NjE2MTk3NTQ2MDQ5NTM3.YIgljQ.zUZF3XTpqlSIgKO9P3yqDWZkn1Y',
         prefix: '-',
         activity: 'ACTIVITY',
     },
