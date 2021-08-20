8705b3c82ac5be0f7ca8340d2ac439ebc4555f0d
Francisco Pulido <67913723+lolways@users.noreply.github.com>
2021-08-20 15:17:29 -0300

    🔴Index v2.0.2

--- index.js
+++ index.js
@@ -19,26 +19,26 @@ fs.readdirSync('./commands').forEach(dirs => {
 
     for (const file of commands) {
         const command = require(`./commands/${dirs}/${file}`);
-        console.log(`Loading command ${file}`);
+        console.log(`Cargando comados ${file}`);
         client.commands.set(command.name.toLowerCase(), command);
     };
 });
 
 // llamar a los eventos y el reproductor 
-const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+const events = fs.readdirSync('./eventos').filter(file => file.endsWith('.js'));
+const player = fs.readdirSync('./Reproductor').filter(file => file.endsWith('.js'));
 
 //uso de eventos
 for (const file of events) {
     console.log(`Loading discord.js event ${file}`);
-    const event = require(`./events/${file}`);
+    const event = require(`./eventos/${file}`);
     client.on(file.split(".")[0], event.bind(null, client));
 };
 
 //uso de player 
 for (const file of player) {
-    console.log(`Loading discord-player event ${file}`);
-    const event = require(`./player/${file}`);
+    console.log(`Cargando Eventos ${file}`);
+    const event = require(`./Reproductor/${file}`);
     client.player.on(file.split(".")[0], event.bind(null, client));
 };
 
