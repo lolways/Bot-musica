2ad7677e3f0ab959fd31f07cee5a1d3ee49ae135
Francisco Pulido <67913723+lolways@users.noreply.github.com>
2021-08-20 15:13:10 -0300

    🔴Config v2.0.2

--- config/bot.js
+++ config/bot.js
@@ -10,7 +10,7 @@ module.exports = {
     discord: {
         token: 'TOKEN',
         prefix: '-',
-        activity: '',
+        activity: 'I hear you',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
